5c257c6a1ee77e9380623bed9b368b92a67a62c6
Robert Shields <robert.shieldsiv@gmail.com>
2020-10-06 18:30:12 -0400

    added lynk landing logig to redirect back to user dashboard if logged in

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -21,6 +21,11 @@ class Register extends Component {
         }
     }
 
+    componentDidMount() {
+      if(this.props.auth.isAuthenticated) {
+        this.props.history.puth('/dashboard')
+      }
+    }
 
     componentWillReceiveProps(nextProps){
       if(nextProps.errors) {
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux'
+import Register from '../auth/Register';
 class Landing extends Component {
+
+  componentDidMount() {
+    if(this.props.auth.isAuthenticated) {
+      this.props.history.puth('/dashboard')
+    }
+  }
     render() {
         return (
             <div className="landing">
@@ -24,4 +32,13 @@ class Landing extends Component {
     }
 }
 
-export default Landing
+Register.propTypes = {
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+
+})
+
+export default connect(mapStateToProps)(Landing)
