7baedf31525b3e1310514b79c2f284fd4c317bf5
Robert Shields <robert.shieldsiv@gmail.com>
2020-10-07 18:34:34 -0400

    added education action and form to state component and redux

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile'
 import AddExperience from './components/add-credentials/AddExperience'
+import AddEducation from './components/add-credentials/AddEducation';
 
 //check for token
 if(localStorage.jwtToken) {
@@ -60,6 +61,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/add-experience" component={ AddExperience } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/add-education" component={ AddEducation } />
               </Switch>
             </div>
             <Footer />
