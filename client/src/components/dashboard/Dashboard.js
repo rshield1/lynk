659c97cf10d514b29ad6c518d6fa45dde00845b8
Robert Shields <robert.shieldsiv@gmail.com>
2020-10-07 00:53:41 -0400

    added spinner component to app

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../actions/profileActions'
+import Spinner from '../common/Spinner'
 
 class Dashboard extends Component {
 
@@ -10,12 +10,36 @@ class Dashboard extends Component {
         
     }
     render() {
+
+        const { user } = this.props.auth;
+        const { profile, loading } = this.props.profile
+
+        let dashboardContent;
+
+        if (profile === null || loading) {
+            dashboardContent = <Spinner />
+        } else {
+            dashboardContent = <h1>Hello</h1>
+        }
+
         return (
-            <div>
-             DASHBOARD   
+            <div className="dashboard">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4">Dashboard</h1>
+                            {dashboardContent}
+                        </div>
+                    </div>
+                </div> 
             </div>
         )
     }
 }
 
-export default connect(null, { getCurrentProfile })(Dashboard)
+const mapStateToProps = (state) => ({
+    profile: state.profile,
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,7 +7,7 @@ class Landing extends Component {
 
   componentDidMount() {
     if(this.props.auth.isAuthenticated) {
-      this.props.history.puth('/dashboard')
+      this.props.history.push('/dashboard')
     }
   }
     render() {
