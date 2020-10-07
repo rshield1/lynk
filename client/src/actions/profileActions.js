64ea5f7d7bc79e450819596c70b720c62dc7f24d
Robert Shields <robert.shieldsiv@gmail.com>
2020-10-07 15:43:51 -0400

    added delete account action to profile actions

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from './types'
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types'
 
 
 //get current profile
@@ -52,4 +52,23 @@ export const createProfile = (profileData, history) => dispatch =>{
                 payload: err.response.data
             })
             )
+}
+
+//delete account and profile 
+
+export const deleteAccount = () => dispatch => {
+    if(window.confirm('Are you Sure?')) {
+        axios
+            .delete('./api/profile')
+            .then(res => 
+                dispatch({
+                    type: SET_CURRENT_USER,
+                    payload: {}
+                })
+            ).catch(err =>
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                }))
+    }
 }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { getCurrentProfile } from '../../actions/profileActions'
+import { getCurrentProfile, deleteAccount } from '../../actions/profileActions'
 import Spinner from '../common/Spinner'
 import { Link } from 'react-router-dom'
 import ProfileActions from './ProfileActions'
@@ -9,7 +9,10 @@ class Dashboard extends Component {
 
     componentDidMount() {
         this.props.getCurrentProfile();
-        
+    }
+
+    onDeleteClick = (e) =>{
+        this.props.deleteAccount()
     }
     render() {
 
@@ -27,6 +30,8 @@ class Dashboard extends Component {
                     <div>
                         <p className="lead text-muted">Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link></p>
                         <ProfileActions />
+                        <div style={{ marginBottom: '60px'}} />
+                        <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger">Delete My Account</button>
                     </div>
                 )
             } else {
@@ -64,4 +69,4 @@ const mapStateToProps = (state) => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard)
